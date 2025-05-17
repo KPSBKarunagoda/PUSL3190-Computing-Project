8d7f90b2ff95b811821d45439f4b7bc68f3ef75b
KPSBKarunagoda <kpsbkarunagoda@students.nsbm.ac.lk>
2025-05-17 18:09:49 +0530

    improved ai analysis responses

--- frontend/js/analysis-details.js
+++
@@ -1,7 +0,0 @@
-/**
- * Redirect script for the removed analysis details page
- */
-(function() {
-  console.log('Analysis details page has been removed. Redirecting to analyze.html...');
-  window.location.href = 'analyze.html';
-})();
